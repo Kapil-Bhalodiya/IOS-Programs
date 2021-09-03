a3f1439a3d2217c3a79ac75d40f7e8b71f15833b
Kapil04 <>
2021-09-03 14:19:06 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,11 @@
+var name:String = "hardik"
+let keys = 10
+print("Hello \(name) you have \(keys) keys")
+var નામ = "સાગર"
+print(નામ)
+let ImplementationError = (501, "Not implemented") 
+print(ImplementationError)
+let a:Int = 5;
+print(a); 
+let a:Optional
+print(a ?? "hey")
